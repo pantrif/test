a0fa9cde820fc5f251ab1b33fa54dbc2ee904aa2
Leonidas Maroulis <leonidas.maroulis@taxibeat.com>
2017-07-06 18:50:50 +0300

    updading travis 4

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -3,4 +3,24 @@
 require dirname(__DIR__) . '/vendor/autoload.php';
 
 
+function testClassLoader($className)
+{
+    $pathPrefix = dirname(__FILE__).'/';
+    $classPath = str_replace('\\', '/', $className).'.php';
+
+    $filename = $pathPrefix.$classPath;
+
+    if (file_exists($filename)) {
+        require_once $filename;
+        if (class_exists($className)) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+// Register our own namespace autoloader and then the one provided by composer
+spl_autoload_register('testClassLoader');
+
 echo "begin test.....\n";
